c9bb7afee2bc72d0a35f71619fbac7a5bb758336
trkang <84390611+mizuharaa@users.noreply.github.com>
2023-03-25 13:16:33 +0700

    Add files via upload

---
+++ timtoado.py
@@ -0,0 +1,11 @@
+xa = int(input('Nhap vao x cua a: '))
+ya = int(input('Nhap vao y cua a: '))
+xb = int(input('Nhap vao x cua b: '))
+yb = int(input('Nhap vao y cua b: '))
+xc = int(input('Nhap vao x cua c: '))
+yc = int(input('Nhap vao y cua c: '))
+xvtab = xb - xa
+yvtab = yb - ya
+xd = xc - xvtab
+yd = yc - yvtab
+print(f"Toa do cua diem D de ABCD la hinh binh hanh co x: {xd} va y: {yd}") 
