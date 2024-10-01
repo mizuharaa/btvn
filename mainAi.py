bab68bb51e05a9482234e6dc8add8c66cd9a530b
trkang <84390611+mizuharaa@users.noreply.github.com>
2024-10-01 10:23:02 +0700

    Update mainAi.py

--- mainAi.py
+++ mainAi.py
@@ -1,1 +1,18 @@
+#Import Libraries
+import cv2
+import os
+import numpy as np
+import random
+from matplotlib import pyplot as plt
 
+capture = cv2.VideoCapture(0)
+
+while True:
+    img = capture.read()
+    cv2.imshow("Facial Detection", img)
+    if cv2.waitKey(1) and 0xFF == ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
+    
